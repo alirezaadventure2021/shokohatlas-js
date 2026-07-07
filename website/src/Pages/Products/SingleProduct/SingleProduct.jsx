83761a856b03dc4a4f283b649c2216b494e42e1f
Alizada <alirezaalizadaafg@gmail.com>
2026-07-07 21:57:37 +0330

    single product error solved

--- website/src/Pages/Products/SingleProduct/SingleProduct.jsx
+++ website/src/Pages/Products/SingleProduct/SingleProduct.jsx
@@ -1,58 +1,53 @@
-import React, { useEffect, useState } from "react";
-import SingleProductTop from "./singleProductTop";
+import { useEffect, useState } from "react";
+import SingleProductTop from "./SingleProductTop";
 import Direction from "../../../Components/Direction";
 import SingleProductContent from "./SingleProductContent";
-import {useNavigate, useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axiosClient from "../../../axios-client";
 import i18n from "../../../i18n";
-import {Helmet} from "react-helmet-async";
 import HelmetComponent from "../../../Components/helmetComponent.jsx";
 
 const SingleProduct = () => {
-
   let { slug } = useParams();
   const [product, setProduct] = useState(null);
-  const navigate= useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
-        axiosClient
-            .get(`/getProduct/${slug}?lang=${i18n.language}`)
-            .then(({ data }) => {
-                setProduct(data);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }, [slug, i18n.language]);
+    axiosClient
+      .get(`/getProduct/${slug}?lang=${i18n.language}`)
+      .then(({ data }) => {
+        setProduct(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [slug, i18n.language]);
 
-    useEffect(() => {
-        if (!product) return;
+  useEffect(() => {
+    if (!product) return;
 
-        const currentLang = i18n.language;
-        const currentSlug = slug;
+    const currentLang = i18n.language;
+    const currentSlug = slug;
 
-        const translatedSlug = product.slug_all?.[currentLang];
+    const translatedSlug = product.slug_all?.[currentLang];
 
-        if (translatedSlug && translatedSlug !== currentSlug) {
-            navigate(`/${currentLang}/products/${translatedSlug}`, { replace: true });
-        }
-    }, [i18n.language]);
+    if (translatedSlug && translatedSlug !== currentSlug) {
+      navigate(`/${currentLang}/products/${translatedSlug}`, { replace: true });
+    }
+  }, [i18n.language]);
 
-    return (
+  return (
     <div>
-        {product && (
-            <div>
-                <HelmetComponent page={product} url="products" />
-                <Direction
-                    link={product?.name}
-                    header={product.name}
-                />
-                <div className="max-w-[1200px] mx-auto mt-[20px]">
-                    <SingleProductTop {...product} />
-                    <SingleProductContent product={product} />
-                </div>
-            </div>
-        )}
+      {product && (
+        <div>
+          <HelmetComponent page={product} url="products" />
+          <Direction link={product?.name} header={product.name} />
+          <div className="max-w-[1200px] mx-auto mt-[20px]">
+            <SingleProductTop {...product} />
+            <SingleProductContent product={product} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
